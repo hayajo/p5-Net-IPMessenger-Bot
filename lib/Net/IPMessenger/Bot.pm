912fef90940e3c61681a651dadfbf947868de3e0
hayajo <hayajo@cpan.org>
2013-05-30 11:59:01 +0900

    0.04

--- lib/Net/IPMessenger/Bot.pm
+++ lib/Net/IPMessenger/Bot.pm
@@ -3,7 +3,7 @@ package Net::IPMessenger::Bot;
 use strict;
 use warnings;
 use 5.008_005;
-our $VERSION = '0.03';
+our $VERSION = '0.04';
 
 use Net::IPMessenger;
 use Net::IPMessenger::Bot::EventHandler;
